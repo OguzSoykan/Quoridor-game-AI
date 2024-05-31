01a87cd942acbfe55d740627b88a8bb5496479a1
ilayda <istikami@mef.edu.tr>
2024-05-31 13:25:42 +0300

    UI
    UI of start screen implemented by PySide6

--- ui_form.py
+++ ui_form.py
@@ -17,31 +17,17 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QHBoxLayout,
     QLabel, QMainWindow, QPushButton, QSizePolicy,
-    QWidget)
+    QSpacerItem, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(399, 346)
+        MainWindow.resize(489, 287)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.player1Image = QLabel(self.centralwidget)
-        self.player1Image.setObjectName(u"player1Image")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.player1Image.sizePolicy().hasHeightForWidth())
-        self.player1Image.setSizePolicy(sizePolicy)
-        self.player1Image.setMinimumSize(QSize(100, 100))
-        self.player1Image.setMaximumSize(QSize(100, 100))
-        self.player1Image.setScaledContents(True)
-        self.player1Image.setAlignment(Qt.AlignCenter)
-
-        self.gridLayout.addWidget(self.player1Image, 0, 0, 1, 1)
-
         self.player1Label = QLabel(self.centralwidget)
         self.player1Label.setObjectName(u"player1Label")
         font = QFont()
@@ -49,19 +35,13 @@ class Ui_MainWindow(object):
         font.setPointSize(14)
         self.player1Label.setFont(font)
 
-        self.gridLayout.addWidget(self.player1Label, 0, 1, 1, 1)
-
-        self.player2Box = QComboBox(self.centralwidget)
-        icon = QIcon(QIcon.fromTheme(u"flag-aa"))
-        self.player2Box.addItem(icon, "")
-        icon1 = QIcon(QIcon.fromTheme(u"applications-games"))
-        self.player2Box.addItem(icon1, "")
-        self.player2Box.setObjectName(u"player2Box")
-
-        self.gridLayout.addWidget(self.player2Box, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.player1Label, 0, 2, 1, 1)
 
         self.player2Image = QLabel(self.centralwidget)
         self.player2Image.setObjectName(u"player2Image")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.player2Image.sizePolicy().hasHeightForWidth())
         self.player2Image.setSizePolicy(sizePolicy)
         self.player2Image.setMinimumSize(QSize(100, 100))
@@ -69,14 +49,27 @@ class Ui_MainWindow(object):
         self.player2Image.setScaledContents(True)
         self.player2Image.setAlignment(Qt.AlignCenter)
 
-        self.gridLayout.addWidget(self.player2Image, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.player2Image, 1, 1, 1, 1)
 
-        self.player1Box = QComboBox(self.centralwidget)
-        self.player1Box.addItem("")
-        self.player1Box.setObjectName(u"player1Box")
-        self.player1Box.setEnabled(False)
+        self.player1Image = QLabel(self.centralwidget)
+        self.player1Image.setObjectName(u"player1Image")
+        sizePolicy.setHeightForWidth(self.player1Image.sizePolicy().hasHeightForWidth())
+        self.player1Image.setSizePolicy(sizePolicy)
+        self.player1Image.setMinimumSize(QSize(100, 100))
+        self.player1Image.setMaximumSize(QSize(100, 100))
+        self.player1Image.setScaledContents(True)
+        self.player1Image.setAlignment(Qt.AlignCenter)
 
-        self.gridLayout.addWidget(self.player1Box, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.player1Image, 0, 1, 1, 1)
+
+        self.player2Box = QComboBox(self.centralwidget)
+        icon = QIcon(QIcon.fromTheme(u"face-smile"))
+        self.player2Box.addItem(icon, "")
+        icon1 = QIcon(QIcon.fromTheme(u"applications-games"))
+        self.player2Box.addItem(icon1, "")
+        self.player2Box.setObjectName(u"player2Box")
+
+        self.gridLayout.addWidget(self.player2Box, 1, 3, 1, 1)
 
         self.player2Label = QLabel(self.centralwidget)
         self.player2Label.setObjectName(u"player2Label")
@@ -86,7 +79,7 @@ class Ui_MainWindow(object):
         font1.setBold(False)
         self.player2Label.setFont(font1)
 
-        self.gridLayout.addWidget(self.player2Label, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.player2Label, 1, 2, 1, 1)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
@@ -105,7 +98,33 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addWidget(self.exitButton)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 3)
+        self.gridLayout.addLayout(self.horizontalLayout, 3, 1, 1, 3)
+
+        self.horizontalSpacer = QSpacerItem(50, 0, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 4, 1, 1)
+
+        self.botImage = QLabel(self.centralwidget)
+        self.botImage.setObjectName(u"botImage")
+        sizePolicy.setHeightForWidth(self.botImage.sizePolicy().hasHeightForWidth())
+        self.botImage.setSizePolicy(sizePolicy)
+        self.botImage.setMinimumSize(QSize(50, 50))
+        self.botImage.setMaximumSize(QSize(50, 50))
+        self.botImage.setScaledContents(True)
+        self.botImage.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout.addWidget(self.botImage, 1, 4, 1, 1)
+
+        self.player1Box = QComboBox(self.centralwidget)
+        self.player1Box.addItem("")
+        self.player1Box.setObjectName(u"player1Box")
+        self.player1Box.setEnabled(False)
+
+        self.gridLayout.addWidget(self.player1Box, 0, 3, 1, 1)
+
+        self.horizontalSpacer_2 = QSpacerItem(50, 0, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer_2, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -116,16 +135,17 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Quoridor Game", None))
-        self.player1Image.setText(QCoreApplication.translate("MainWindow", u"Player1Image", None))
         self.player1Label.setText(QCoreApplication.translate("MainWindow", u"Player 1", None))
+        self.player2Image.setText(QCoreApplication.translate("MainWindow", u"Player2Image", None))
+        self.player1Image.setText(QCoreApplication.translate("MainWindow", u"Player1Image", None))
         self.player2Box.setItemText(0, QCoreApplication.translate("MainWindow", u"Human", None))
         self.player2Box.setItemText(1, QCoreApplication.translate("MainWindow", u"Bot", None))
-
-        self.player2Image.setText(QCoreApplication.translate("MainWindow", u"Player2Image", None))
-        self.player1Box.setItemText(0, QCoreApplication.translate("MainWindow", u"Human", None))
 
         self.player2Label.setText(QCoreApplication.translate("MainWindow", u"Player 2", None))
         self.startButton.setText(QCoreApplication.translate("MainWindow", u"Start", None))
         self.exitButton.setText(QCoreApplication.translate("MainWindow", u"Exit", None))
+        self.botImage.setText(QCoreApplication.translate("MainWindow", u"botImage", None))
+        self.player1Box.setItemText(0, QCoreApplication.translate("MainWindow", u"Human", None))
+
     # retranslateUi
 
